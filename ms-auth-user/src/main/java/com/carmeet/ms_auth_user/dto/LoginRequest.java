7ed35563fa82a3f42c4b9141ab0db0108176863b
Giulianno Inostroza <giu.inostroza@duocuc.cl>
2026-06-01 23:57:06 -0400

    Fix: Constructor

--- ms-auth-user/src/main/java/com/carmeet/ms_auth_user/dto/LoginRequest.java
+++ ms-auth-user/src/main/java/com/carmeet/ms_auth_user/dto/LoginRequest.java
@@ -1,9 +1,13 @@
 package com.carmeet.ms_auth_user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank(message = "Username es obligatorio")
